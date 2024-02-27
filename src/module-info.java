2ba9eac0eff967e7effcb8ff63bef44c6b50f703
Eder Vinicius <eder.e10@hotmail.com>
2024-02-27 10:20:55 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -2,9 +2,12 @@ module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	
 	exports gui to javafx.fxml;     
-	opens gui to javafx.fxml; 
+	opens gui to javafx.fxml; 	
+	
+	opens model.entities to javafx.base;
 }
